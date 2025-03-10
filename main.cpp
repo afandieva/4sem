e44fde0e6a7043982f1c3046b8df7cec2fb326c4
Emilia Afandieva <153540378+afandieva@users.noreply.github.com>
2025-03-10 11:48:33 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
-#include <iostream>
+/*#include <iostream>
+#include <thread>
 
-union student {
-    int age;
-    float averageScore;
-    char* name;
+
+void foo(int z) {
+    for(int i = 0; i != 0 ; i++)
+        std::cout<<"Thread using function foo\n";
+}
+class thread_obj {
+public:
+    void operator()(int x) {
+        for(int i = 0; i != x ; i++ )
+            std::cout<<"Thread using thread_obj\n";
+    }
 };
-/*int main() {
-    student shaman;
-    shaman.age = 15;
-    std::cout<<"shaman age: "<<shaman.age<<"\n";
-    shaman.averageScore = 2.5;
-    std::cout<<"shaman score: "<<shaman.averageScore<<"\n";
-    shaman.name = "ya russkiy";
-    std::cout<<"shaman name: "<<shaman.name<<"\n";
-    std::cout<<"Address 1: "<<&shaman.age<<"\n";
-    std::cout<<"Address 2: "<<&shaman.averageScore<<"\n";
-    std::cout<<"Address 3: "<<&shaman.name<<"\n";
-    std::cout<<"Size of what???: "<<sizeof(shaman)<<"\n";
-
+class Base {
+public:
+    void foo() {
+        std::cout<<"Thread using member function foo()\n";
+    }
+    static void foo1() {
+        std::cout<<"Thread using static member function foo1()\n";
+    }
+};
+int main() {
+    std::thread t1(foo,3);
+    std::thread t2(thread_obj(),3);
+    auto f = [](int x) {
+        for(int i = 0; i != x; ++i)
+            std::cout<<"Thread using lambda\n";
+    };
+    std::thread t3(f,3);
+    Base b;
+    std::thread t4(&Base::foo,&b);
+    std::thread t5(&Base::foo1);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
 }*/
